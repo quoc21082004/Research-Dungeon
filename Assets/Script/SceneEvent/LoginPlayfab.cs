0637b640f09ce8b4152239898db32addf53fa925
quoc21082004 <quoc21082004@gmail.com>
2024-02-11 19:03:06 +0700

    123

--- Assets/Script/SceneEvent/LoginPlayfab.cs
+++ Assets/Script/SceneEvent/LoginPlayfab.cs
@@ -1,18 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
+using PlayFab;
 public class LoginPlayfab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI loginTitle_txt;
+    [Header("       Login")]
+    [SerializeField] GameObject loginprefab;
+    [SerializeField] TMP_InputField loginInput;
+    [SerializeField] TMP_InputField loginPasswordInput;
 
-    // Update is called once per frame
-    void Update()
+    [Header("       Register")]
+    [SerializeField] GameObject registerprefab;
+    [SerializeField] TMP_InputField registerInput;
+    [SerializeField] TMP_InputField registerPasswordInput;
+
+    private void OnEnable()
     {
-        
+        loginTitle_txt.gameObject.SetActive(false);
+    }
+    public void OnLoginPage()
+    {
+        loginTitle_txt.text = "Login";
+        loginprefab.gameObject.SetActive(true);
+        registerprefab.gameObject.SetActive(false);
+        loginTitle_txt.gameObject.SetActive(true);
+    }
+    public void OnRegisterPage()
+    {
+        loginTitle_txt.text = "Register";
+        registerprefab.gameObject.SetActive(true);
+        loginprefab.gameObject.SetActive(false);
+        loginTitle_txt.gameObject.SetActive(true);
+    }
+    public void OnRecoverPage()
+    {
+
+    }
+    public void RegisterUser()
+    {
+        var request = new Reg
     }
 }
