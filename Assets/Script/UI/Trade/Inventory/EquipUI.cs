f923bab44798ff0eec89ddd4047ebf4dda4fb401
quoc21082004 <quoc21082004@gmail.com>
2024-03-29 09:27:43 +0700

    Equipment

---
+++ Assets/Script/UI/Trade/Inventory/EquipUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class EquipUI : InventoryUI
+{
+    int numOfEquipment;
+    protected override void Awake()
+    {
+        numOfEquipment = Enum.GetNames(typeof(EquipmentSlot)).Length;
+        for (int i = 0; i < numOfEquipment; i++) // head- chest - leg - feet
+        {
+            var spawnSlot = PoolManager.instance.Release(slotprefab.gameObject);
+            spawnSlot.transform.SetParent(itemsParent);
+            spawnSlot.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>().ToList();
+    }
+    protected override void UpdateUI()
+    {
+        if (selectedItem != null && selectedItem.currentAmt <= 0)
+            selectedItem = null;
+        selectItemDisplay.UpdateUI();
+        gold_text.text = "" + inventory.Gold;
+        //inventory.onItemChangedCallBack += Equip;
+    }
+    private void Update() => Equip();
+    public void Equip()
+    {
+        for (int i = 0; i < slots.Count; i++)
+        {
+            if (EquipmentManager.instance.currentEquipment[i] != null)
+            {
+                slots[i].AddItem(EquipmentManager.instance.currentEquipment[i], i);
+                slots[i].stackItem_text.enabled = false;
+                slots[i].GetComponent<EquipSlot>().GetItemValue = i; 
+            }
+            else if (EquipmentManager.instance.currentEquipment[i] == null)       // unequip
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
---
+++ Assets/Script/UI/Trade/Shop/EquipItemOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EquipItemOptions : ItemOptions
+{
+    public Button unEquip_btn;
+    int equipmentIndex;
+    private void OnEnable()
+    {
+        unEquip_btn.Select();
+        unEquip_btn.OnSelect(null);
+    }
+    private void Update()
+    {
+        if (selectSlotbtn != null)
+        {
+            equipmentIndex = selectSlotbtn.GetComponentInParent<EquipSlot>().GetItemValue;
+        }
+    }
+    public void OnUnEquipButton()
+    {
+        EquipmentManager.instance.UnEquip(equipmentIndex);
+        OnBackButton();
+        gameObject.SetActive(false);
+    }
+}
